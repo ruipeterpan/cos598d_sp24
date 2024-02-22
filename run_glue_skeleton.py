02cd0fdc4262a43fc60e723256d884647ec2b24c
Niushaaa <n76.moshrefi@gmail.com>
2024-02-21 20:22:43 -0500

    task2a log added

--- run_glue_skeleton.py
+++ run_glue_skeleton.py
@@ -144,23 +144,20 @@ def train(args, train_dataset, model, tokenizer):
                 for i, param in enumerate(model.parameters()):
                     print(f"Rank {torch.distributed.get_rank()} is gathering gradients for param {i}")
                     torch.distributed.gather(param.grad.data, gather_list=gathered_grads[i], dst=0)
+                # Average gradients
+                if torch.distributed.get_rank() == 0:
+                    print(f"Rank {torch.distributed.get_rank()} is averaging gradients")
+                    averaged_grads = [grad / torch.distributed.get_world_size() for grad in gathered_grads]
 
-                gathered_grads = [torch.zeros_like(param.grad.data) for param in model.parameters()]
+                # Scatter averaged gradients back to all processes
+                for i, param in enumerate(model.parameters()):
+                    print(f"Rank {torch.distributed.get_rank()} is scattering gradients for param {i}")
+                    torch.distributed.scatter(param.grad.data, scatter_list=averaged_grads[i], src=0)
+            else:
+                # Worker process
                 for i, param in enumerate(model.parameters()):
                     print(f"Rank {torch.distributed.get_rank()} is gathering gradients for param {i}")
-                    # Use gather_list as a list of tensors
-                    torch.distributed.gather(param.grad.data, gather_list=gathered_grads[i], dst=0)
-
-            # Average gradients
-            if torch.distributed.get_rank() == 0:
-                print(f"Rank {torch.distributed.get_rank()} is averaging gradients")
-                averaged_grads = [grad / torch.distributed.get_world_size() for grad in gathered_grads]
-
-            # Scatter averaged gradients back to all processes
-            for i, param in enumerate(model.parameters()):
-                print(f"Rank {torch.distributed.get_rank()} is scattering gradients for param {i}")
-                torch.distributed.scatter(param.grad.data, scatter_list=averaged_grads[i], src=0)
-
+                    torch.distributed.gather(param.grad.data, dst=0)
 
             tr_loss += loss.item()
             if (step + 1) % args.gradient_accumulation_steps == 0:

3ce982d731cbb396d37d5c4e7b009ced7bbef8c9
Niushaaa <n76.moshrefi@gmail.com>
2024-02-21 19:33:04 -0500

    task2a log added

--- run_glue_skeleton.py
+++ run_glue_skeleton.py
@@ -390,10 +390,10 @@ def main():
         raise ValueError("Output directory ({}) already exists and is not empty. Use --overwrite_output_dir to overcome.".format(args.output_dir))
 
     # set up (distributed) training
-    args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
-    args.n_gpu = torch.cuda.device_count()
+    print("kharrr")
     torch.distributed.init_process_group(rank=args.local_rank, world_size=torch.distributed.get_world_size(), backend="gloo", init_method="file:///tmp/somefile", group_name="pytorch-test")
-    
+    args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    args.n_gpu = torch.cuda.device_count()    
 
     # Setup logging
     logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
